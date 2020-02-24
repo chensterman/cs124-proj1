84dd6ded286f2e7d7ec03ee6d287899d63a81fc5
Helen Pang <helenlpang@outlook.com>
2020-02-24 11:23:31 -0500

    bin heap is good to go

--- randmst2.java
+++ randmst2.java
@@ -1,20 +1,30 @@
 public class randmst2 {
+
+  public static void traverse_0 (int numpoints, int numtrials) {
+    MinHeap minHeap = new MinHeap(numpoints); 
+    boolean[] traversed = new boolean[numpoints];
+    traversed[0] = true;
+    
+  }
+
+  public static void traverse(int numpoints, int numtrials, int dimension) {
+
+  }
   public static void main(String[] arg) 
   { 
-      System.out.println("The Min Heap is "); 
-      MinHeap minHeap = new MinHeap(15); 
-      minHeap.insert(0,5); 
-      minHeap.insert(1, 3); 
-      minHeap.insert(2,17); 
-      minHeap.insert(2,10); 
-      minHeap.insert(2,1); 
-      minHeap.insert(2,2); 
-      minHeap.insert(2,4); 
-      minHeap.insert(2,22); 
-      minHeap.insert(8,9); 
-      minHeap.minHeap(); 
-
-      minHeap.print(); 
-      System.out.println("The Min val is " + minHeap.remove()); 
+      traverse_0(5, 0);
+      // minHeap.insert(0,5); 
+      // minHeap.insert(1, 3); 
+      // minHeap.insert(2,17); 
+      // minHeap.insert(2,10); 
+      // minHeap.insert(2,1); 
+      // minHeap.insert(2,2); 
+      // minHeap.insert(2,4); 
+      // minHeap.insert(2,22); 
+      // minHeap.insert(8,9); 
+      // minHeap.minHeap(); 
+   
+      // minHeap.print(); 
+      // System.out.println("The Min val is " + minHeap.remove()); 
   } 
 }
